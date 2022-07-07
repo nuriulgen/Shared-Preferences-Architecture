7ae80261ab875cabd27e305f8b60a0bfe9f257b6
nuriulgen <bazid04ulgen@gmail.com>
2022-07-07 03:30:15 +0300

    Create Custom Rich Text

---
+++ lib/core/components/button/custom_rich_text.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+
+import '../../../product/color/color_items.dart';
+
+class CustomRichText extends StatelessWidget {
+  CustomRichText({
+    Key? key,
+    required this.title,
+    this.onPressed,
+    required this.buttonTitle,
+  }) : super(key: key);
+  ColorItems colorItems = ColorItems();
+  final String title;
+  final String buttonTitle;
+
+  final VoidCallback? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+          text: title,
+          style: Theme.of(context)
+              .textTheme
+              .subtitle1
+              ?.copyWith(color: colorItems.bodyColor),
+          children: <TextSpan>[
+            TextSpan(
+                text: buttonTitle,
+                style: Theme.of(context)
+                    .textTheme
+                    .subtitle1
+                    ?.copyWith(color: colorItems.buttonColor),
+                recognizer: TapGestureRecognizer()..onTap = onPressed)
+          ]),
+    );
+  }
+}
