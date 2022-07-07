d4958837517d2ababf5f23556750ef6f5f09331d
nuriulgen <bazid04ulgen@gmail.com>
2022-07-07 22:22:07 +0300

    Create height-width extension

---
+++ lib/core/extension/size_extenison.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+extension SizeExtension on BuildContext {
+  double get height => MediaQuery.of(this).size.height;
+  double get width => MediaQuery.of(this).size.width;
+
+  double get lowValue => height * 0.01;
+  double get normalValue => height * 0.02;
+  double get mediumValue => height * 0.04;
+  double get highValue => height * 0.1;
+}
