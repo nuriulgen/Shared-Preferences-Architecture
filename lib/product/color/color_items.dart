969be717009d72830774ec57f5efd886c454f27a
nuriulgen <bazid04ulgen@gmail.com>
2022-07-07 03:31:27 +0300

    Create ColorItems class.

---
+++ lib/product/color/color_items.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorItems {
+  final Color bodyColor = const Color(0xFF7B7B8B);
+  final Color inputBackgroundColor = const Color(0xFF1D192C);
+  final Color inputColor = const Color(0xFF7477A0);
+  final Color buttonColor = const Color(0xFFFB6580);
+  final Color facebookButtonColor = const Color(0xFF39579A);
+  final Color whiteColor = Colors.white;
+  final Color blackColor = Colors.black87;
+}
